1314335f9cc28929be054b241208107eef160209
jumbotsang <xiaotiaody@gmail.com>
2026-09-07 23:04:55 +0800

    Update diy.sh

--- JP-TVbox/diy.sh
+++ JP-TVbox/diy.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
-#!/bin/bash
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
 
 # Default IP
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
 # Add packages
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
+git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki package/luci-app-nikki
 
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-argon-config
+# Clean packagesrm -rf clone
