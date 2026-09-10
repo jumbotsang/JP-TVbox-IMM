e0d05c034be038cfa0a6e4ca52abaaefb921fd99
jumbotsang <xiaotiaody@gmail.com>
2026-09-10 15:04:46 +0800

    Update diy.sh

--- JP-TVbox/diy.sh
+++ JP-TVbox/diy.sh
@@ -4,7 +4,7 @@
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
 # Add packages
-git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki package/luci-app-nikki
 
 # Clean packagesrm -rf clone
